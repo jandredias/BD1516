4766fcb4ea860b78ab3a4ab1e1b672d8f1c75909
Miguel Amaral <miguel.amaral04@gmail.com>
2015-12-07 12:49:27 +0000

    a

--- projecto/restricoes_integridade.sql
+++ projecto/restricoes_integridade.sql
@@ -5,22 +5,8 @@ uma vez no universo das relaçõoes tipo registo, pagina, campo, registo e valor
    ---------------------------------------------------------------------------
    ---------------------------------------------------------------------------
 */
-drop PROCEDURE IF EXISTS occurences_in_table;
-Delimiter //
-CREATE  PROCEDURE `occurences_in_table`(IN tab_name VARCHAR(60), IN idseq_value int , OUT number_ocurrences int )
-BEGIN
-
- SET @table = tab_name;
- SET @seq = idseq_value;
- SET @t1 =CONCAT('SELECT count(*) as times FROM ',@table,' t WHERE t.idseq = ',@seq ,' into number_ocurrences');
- PREPARE stmt3 FROM @t1;
- EXECUTE stmt3;
- DEALLOCATE PREPARE stmt3;
-END //
-Delimiter ;
 
 
-call occurences_in_table('pagina' , '151489', value);
 
 
 drop function IF EXISTS consulting;
@@ -36,6 +22,56 @@ Delimiter ;
 
 
 
+drop function IF EXISTS consulting;
+Delimiter //
+create function consulting (new_idseq int)
+  returns int
+begin
+	DECLARE rowsCount int;
+	DECLARE number_ocurrences int;
+	set rowsCount = 0;
+	/*pagina*/
+	SELECT count(*)
+	FROM pagina t
+	WHERE t.idseq = new_idseq
+	into number_ocurrences;
+	set rowsCount = rowsCount + number_ocurrences;
+
+	/*tipo_registo*/
+	SELECT count(*)
+	FROM tipo_registo t
+	WHERE t.idseq = new_idseq
+	into number_ocurrences;
+	set rowsCount = rowsCount + number_ocurrences;
+
+	/*registo*/
+	SELECT count(*)
+	FROM registo t
+	WHERE t.idseq = new_idseq
+	into number_ocurrences;
+	set rowsCount = rowsCount + number_ocurrences;
+
+	/*campo*/
+	SELECT count(*)
+	FROM campo t
+	WHERE t.idseq = new_idseq
+	into number_ocurrences;
+	set rowsCount = rowsCount + number_ocurrences;
+
+	/*valor*/
+	SELECT count(*)
+	FROM valor t
+	WHERE t.idseq = new_idseq
+	into number_ocurrences;
+	set rowsCount = rowsCount + number_ocurrences;
+
+	return rowsCount;
+END //
+Delimiter ;
+
+select  consulting('151489');
+
+
 
 
 Delimiter //
@@ -43,25 +79,11 @@ CREATE TRIGGER upd_tables BEFORE UPDATE ON pagina
 FOR EACH ROW
 BEGIN
   DECLARE rowsCount int;
-  DECLARE value1     int;
-  DECLARE value2     int;
-  DECLARE value3     int;
-  DECLARE value4     int;
-  DECLARE value5     int;
-  DECLARE msg VARCHAR(255);
-  call occurences_in_table('pagina'       , new.idseq, value1);
-  set rowsCount = rowsCount + value1;
-  call occurences_in_table('tipo_registo' , new.idseq, value2);
-  set rowsCount = rowsCount + value2;
-  call occurences_in_table('registo'      , new.idseq, value3);
-  set rowsCount = rowsCount + value3;
-  call occurences_in_table('campo'        , new.idseq, value4);
-  set rowsCount = rowsCount + value4;
-  call occurences_in_table('valor'        , new.idseq, value5);
-  set rowsCount = rowsCount + value5;
+  DECLARE msg VARCHAR(512)
+  set rowsCount = select consulting(new.idseq);
 
 
-  IF rowsCount > 0 THEN
+  IF rowsCount <> 0 THEN
   	  set msg = "DIE: You broke the rules... I will now Smite you, hold still...";
       SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = msg;
       /*ROLLBACK;*/
@@ -69,9 +91,6 @@ BEGIN
 END //
 Delimiter ;
 
-  SELECT COUNT(*)
-  FROM pagina
-  WHERE idseq is :new.idseq INTO rowsCount;
 
 -- CHECK THIS TABLES
 tipo_registo
