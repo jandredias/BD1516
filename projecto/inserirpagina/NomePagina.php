679de1793a5cacacc4fcbdee7d39f48466314701
Miguel Carvalho <miguel_carvalho_@hotmail.com>
2015-12-07 15:34:41 +0000

    Small change, "Balance" to "Novo nome"

--- projecto/inserirpagina/NomePagina.php
+++ projecto/inserirpagina/NomePagina.php
@@ -3,7 +3,7 @@
         <h3>Change balance for user <?=$_REQUEST['userid']?> and email <?=$_REQUEST['email']?> </h3>
         <form action="updatePagina.php?userid=<?=$_REQUEST['userid']?>&email=<?=$_REQUEST['email']?>" method="post">
             <p></p>
-            <p>New balance: <input type="text" name="nome"/></p>
+            <p>Nome para a nova pagina: <input type="text" name="nome"/></p>
             <p><input type="submit" value="Submit"/></p>
         </form>
     </body>
