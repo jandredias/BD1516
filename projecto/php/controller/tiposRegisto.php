6bd5fe3a043fcc2148fc57d454689a32ca0d3495
Miguel Silva Mateus Belo de Carvalho <miguel.s.carvalho@tecnico.ulisboa.pt>
2015-12-09 15:51:26 +0000

    Remove registo done except testing xD

--- projecto/php/controller/tiposRegisto.php
+++ projecto/php/controller/tiposRegisto.php
@@ -11,6 +11,27 @@ if($_GET['page'] == "tiposRegisto" && isset($_GET['accao'])){
     case "remove":
       if(isset($_GET['tid'])){
         //FIXME
+         
+		$userid = $this->user->userid;
+		$tid=$_GET['tid'];
+		$date=(date('Y-m-d H:i:s'));
+		/* BEGIN TRANSACTION */
+		$connection->begintransaction();
+		/*$query = $connection->prepare(
+		  "INSERT INTO sequencia(userid,moment)
+		   VALUES (:userid,:date);");
+		$query->execute(array(':userid' => $userid, ':date' => $date));
+		*/
+		$query = $connection->prepare(
+		  "UPDATE tipo_registo 
+		   SET ativo=0 
+		   WHERE userid=:userid 
+		   AND typecnt=:typecnt;");
+		$query->execute(array(':userid' => $userid,':pagecounter' => $tid));
+
+		$connection->commit();
+		/* END TRANSACTION */
+			
       }
       break;
     default:
